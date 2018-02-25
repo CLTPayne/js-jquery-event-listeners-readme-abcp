3758e7e01cee7ac85767035da93a8a4a8a8f2046
Charlotte Payne <cltpayne@gmail.com>
2018-02-25 18:39:51 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,21 +1,4 @@
-//define functions here
-function getIt () {
-  $('p').on('click', function() {
-    alert("Hey!")
-  })
-}
 
-function frameIt () {
-  $('img').on('load', function () {
-     $('img').addClass('tasty')
-  })
-}
-
-function pressIt () {
-  $('input').on('keydown', function () {
-    alert()
-  }) 
-}
 
 $(document).ready(function(){
 
